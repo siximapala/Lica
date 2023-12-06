bfa21ca0b24f7f67cb7178fbdaeeef2cf7711e96
siximapala <110502366+siximapala@users.noreply.github.com>
2023-12-06 21:04:08 +0500

    Добавление функций грабa oAuth и отправка в back

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,40 @@
-import {useState/*создание переменной useState, для получения данных из бэкенда, рендерить данные на странице*/,
- useEffect /*получать бэкенд API для рендера*/} from 'react';
+import { useState } from 'react';
 
 export default function App() {
-  
-const [data, setData] = useState([{}])
-useEffect(() =>{
-    fetch("/members").then(
-      
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+  const client_id = '111039d461f9409a8bfabcbbbdba725b';
+  const handleClick = () => {
+    const oauthUrl = 'https://oauth.yandex.ru/authorize?response_type=token&client_id='+client_id;
+    window.open(oauthUrl, '_blank');
+  };
 
+
+  function handleSubmit(token) {
+    // Чтобы браузер не перезагрузил страницу
+    token.preventDefault();
+
+    // Читает данные из формы, в которую положили ключ
+    const form = token.target;
+    const formData = new FormData(form);
+
+    //в консоле в браузере можно проверить отправку
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    
+    //здесь идёт отправка на некоторый адрес flask
+    fetch('/some-api', { method: form.method, body: formData });
+  }
 
   return (
     <div>
-    {(typeof data.members === 'undefined') ? (<p>
-      Loading...
-    </p>) : (data.members.map((member, i) =>
-    (
-      <p key={i}> {member}</p>
-    ))
-    )}
-  </div>
+      
+        <form method="post" onSubmit={handleSubmit}>
+        <button onClick={handleClick}>Авторизоваться через Яндекс ID</button>,
+        <p>Нажатие на кнопку переводит на сайт, где нужно авторизироваться через ЯндексID</p>
+        <p>Скопируйте OAuth токен, после чего можете закрыть страницу. Введите его в поле ниже.</p>
+        <label> Введите Oauth токен сюда: <input name="yadiskAPIToken" /> </label>
+        <button type="submit">Подтвердить</button>
+        </form>
+      
+    </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
+import yadisk
 
 app = Flask(__name__)
-CORS(app)
-
 
 @app.route("/members")
 def members():
