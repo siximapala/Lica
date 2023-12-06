68e3cf42a6d524d4bb51439de44255c8dfc3ccc3
siximapala <110502366+siximapala@users.noreply.github.com>
2023-12-06 22:31:32 +0500

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [data, setData] = useState('');
   const client_id = '111039d461f9409a8bfabcbbbdba725b';
+
   const handleClick = () => {
     const oauthUrl = 'https://oauth.yandex.ru/authorize?response_type=token&client_id='+client_id;
     window.open(oauthUrl, '_blank');
@@ -20,17 +23,20 @@ export default function App() {
     const formJson = Object.fromEntries(formData.entries());
     console.log(formJson);
     
-    //здесь идёт отправка на некоторый адрес flask
-    fetch('/some-api', { method: form.method, body: formData });
+    //здесь идёт пост этих данных на адрес сервера React
+    axios.post('/data', {data: formJson})
+      .then(response => setData(response.data))
+      .catch(error => console.error(error));
   }
 
   return (
     <div>
       
-        <form method="post" onSubmit={handleSubmit}>
+        
         <button onClick={handleClick}>Авторизоваться через Яндекс ID</button>,
         <p>Нажатие на кнопку переводит на сайт, где нужно авторизироваться через ЯндексID</p>
         <p>Скопируйте OAuth токен, после чего можете закрыть страницу. Введите его в поле ниже.</p>
+        <form method="post" onSubmit={handleSubmit}>
         <label> Введите Oauth токен сюда: <input name="yadiskAPIToken" /> </label>
         <button type="submit">Подтвердить</button>
         </form>
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import yadisk
 
 app = Flask(__name__)
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route("/members")
 def members():
     return {"members":["Member1", "Member2", "Member3"]}
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.json.get('data')
+    # Обработка полученных данных
+    print(data) 
+    return jsonify({'message': f'Received data: {data}'})
 
 if __name__ == "__main__":
     app.run(debug=True)
