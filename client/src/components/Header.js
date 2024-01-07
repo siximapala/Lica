bcf1fb5ded9705110d7b0c9002bf0c3950f3cd9c
siximapala <110502366+siximapala@users.noreply.github.com>
2024-01-07 15:26:38 +0500

    Сэйвлю недоделанный мультипейдж из-за проблем с компом

---
+++ client/src/components/Header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <h1>heder epta </h1>
+    )
+}
---
+++ client/src/pages/convert.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Convert(){
+  return(
+    <>
+    <Header/>
+    <Container maxWidth="sm">
+      <h2>convert page</h2>
+    </Container>
+    
+    </>
+  )
+}
