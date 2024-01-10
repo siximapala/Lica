5485423a2b28971c7e13317f322a6d8403d0f67c
Dmitry <dsterligov2004@mail.ru>
2024-01-10 16:21:07 +0500

    обновлены методы в yadisk_class

--- flask-server/yadisk_class.py
+++ flask-server/yadisk_class.py
@@ -1,5 +1,5 @@
 import yadisk
-
+import os
 class I_yadisk:
 
     def __init__(self, token, client_id = "80962dc658884279a549db8f33953722", secret_id = "d94d84dd23a6472d81b77aa8d121778f"):
@@ -19,7 +19,7 @@ class I_yadisk:
     def update_file(self, file):
         if self.check():    
             if self.y.is_file("/Lica/" + file):
-                self.y.remove(file, permanently=False)
+                self.y.remove(str("/Lica/" + file), permanently=True)
                 self.y.upload(str(file), str("/Lica/" + file))
             else:
                 self.y.upload(str(file), str("/Lica/" + file))
@@ -35,23 +35,35 @@ class I_yadisk:
     def get_files_name(self, path):
         dict_files = dict()
         for item in self.y.listdir(path):
-            dict_files[item['name']] = {
-                "Название": item['name'],
-                "Размер": item["size"],
-                "Тип файла": item['type'],
-                "Тип документа": item['media_type'],
-                "Дата создания": item['created']
-            }  
+            if item['type'] == 'dir':
+                dict_files[item['name']] = {
+                    "Название": item['name'],
+                    "Размер": item["size"],
+                    "Тип файла": item['type'],
+                    "Тип документа": item['media_type'],
+                    "Дата создания": item['created'],
+                    "Вложенные файлы": self.get_files_name(path + "/" + item['name'])
+                }  
+            else:
+                dict_files[item['name']] = {
+                    "Название": item['name'],
+                    "Размер": item["size"],
+                    "Тип файла": item['type'],
+                    "Тип документа": item['media_type'],
+                    "Дата создания": item['created']
+                }  
         return dict_files
 
     def download_from_disk(self, file):
         if self.check():
-            if self.y.is_file(file):
-                self.y.download(file, file) 
+            self.y.download(str("/Lica/" + file), str(file)) 
+            print("произведена установка файла:" + file)
+            self.dict_files_upload.append(file)
         
     def __del__(self):
         for file in self.dict_files_upload:
             self.update_file(file)
+            print(self.dict_files_upload)
             os.remove(file)
         # with open("cashe_nameworkfiles.txt") as file:
         #     for i in self.dict_files_upload:
@@ -66,5 +78,6 @@ f = I_yadisk("y0_AgAAAABF_QebAAr1PwAAAAD0J7_iFf1mi2K1TICY7B-19b5bS3gXuzE")
 
 r = f.get_files_name("Lica")
 print(r)
-f.update_file('test.txt')
-f.download_from_disk('test.txt')
+f.update_file("test.txt")
+f.download_from_disk("test.txt")
+del f
